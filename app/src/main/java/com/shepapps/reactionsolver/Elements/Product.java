ece6edb1df966adac6e04cb21e993796308e9cf6
shepherdsc <scottjshep@gmail.com>
2016-11-13 09:20:36 -0500

    Initial commit

---
+++ app/src/main/java/com/shepapps/reactionsolver/Elements/Product.java
@@ -0,0 +1,7 @@
+package com.shepapps.reactionsolver.Elements;
+
+/**
+ * Created by scott_000 on 11/10/2016.
+ */
+public class Product {
+}
---
+++ app/src/main/java/com/shepapps/reactionsolver/MainScreen.java
@@ -0,0 +1,28 @@
+package com.shepapps.reactionsolver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+    }
+
+    public void toSelect(View view){
+
+    }
+
+    public void goToReaction(View view){
+
+    }
+
+    public void goToPT(View view){
+
+    }
+}
