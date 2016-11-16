196967ad7ef2bc265436aedfe81a3a34fe3cd640
Richard <richardliu2001@gmail.com>
2016-11-16 13:45:36 -0500

    Added Matrix Class

--- app/src/main/java/com/shepapps/reactionsolver/Elements/Matrix.java
+++ app/src/main/java/com/shepapps/reactionsolver/Elements/Matrix.java
@@ -4,8 +4,6 @@ package com.shepapps.reactionsolver.Elements;
  * Created by Richard on 11/16/2016.
  */
 
-public class Reaction {
-    int[] coefficients;
-    Species[] REactionSpecies;
+public class Matrix {
 
 }
